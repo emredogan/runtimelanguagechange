a0950ea9f9d5653c09a84f644d17c3fca03327c0
Emre Dogan <emre.dogan@shortcut.no>
2023-02-09 23:29:37 +0100

    Initial Commit

---
+++ RuntimeLanguageChange/RuntimeLanguageChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RuntimeLanguageChangeApp.swift
+//  RuntimeLanguageChange
+//
+//  Created by Emre Dogan on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct RuntimeLanguageChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
